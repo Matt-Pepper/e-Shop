872f67f755df4d64036e343eecbc290019c8bd82
Matt-Pepper <matthew.g.pepper@gmail.com>
2022-12-01 10:06:59 +1100

    added sreenshots

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDoEBXkPKYAa0EGhctE_eb6sQoj9Dy-XhI",
-  authDomain: "nology-react.firebaseapp.com",
-  projectId: "nology-react",
-  storageBucket: "nology-react.appspot.com",
-  messagingSenderId: "289627186353",
-  appId: "1:289627186353:web:3b741f50b5b16d7492ae90",
-  measurementId: "G-D7PSJPQ33T"
+	apiKey: "AIzaSyB6CQ6F6E8YkJstbu-vnZJDWTHh822c34Q",
+	authDomain: "nology-react.firebaseapp.com",
+	projectId: "nology-react",
+	storageBucket: "nology-react.appspot.com",
+	messagingSenderId: "289627186353",
+	appId: "1:289627186353:web:3b741f50b5b16d7492ae90",
+	measurementId: "G-D7PSJPQ33T",
 };
 
 // Initialize Firebase
